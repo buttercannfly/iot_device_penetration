85db45434297b57c218ceed4bc13c43fb5e7864e
nut <butter>
2020-03-10 10:56:59 +0800

    firmware scan

--- info_gather/port_scanner.py
+++ info_gather/port_scanner.py
@@ -1,8 +1,16 @@
-import nmap3
+import nmap
 
 def port_scan(ip):
-    nmap = nmap3.Nmap()
-    results = nmap.nmap_os_detection(ip)
-    print(results)
-    return results
+    nm = nmap.PortScanner()
+    nm.scan(ip,'22-443')
+    for host in nm.all_hosts():
+        print('Host: %s(%s)'%(host,nm[host].hostname()))
+        print('State:%s'%nm[host].state())
+        for proto in nm[host].all_protocols():
+            print("Protocol:%s"%proto)
+            lport = nm[host][proto].keys()
+            for port in lport:
+                print('port : %s\tstate : %s' % (port, nm[host][proto][port]['state']))
+
+
 port_scan('127.0.0.1')
